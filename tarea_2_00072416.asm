4d8666eb9ddfbbec7224f80603880c179003c2fb
David Massana <00072416@uca.edu.sv>
2020-04-26 20:44:16 -0600

    Ejercicio 1 hecho

--- tarea_2_00072416.asm
+++ tarea_2_00072416.asm
@@ -1,9 +1,9 @@
 ;Desarrollo Ejercicio 1
 org 100h;
 
-;guardamos las notas
+;limpiamos el registro
 mov ax, 0d
-
+;guardamos las notas
 add ax, 7d
 add ax, 2d
 add ax, 4d
@@ -11,12 +11,27 @@ add ax, 1d
 add ax, 6d
 
 mov cl, 5d
+;obtenemos el promedio
 div cl
+
+;loop para escribir en memoria dinamicamente
+mov di, 0d ;di: apuntador para contar
+mov cx, [length]
+move:
+    mov bh, [comment+di]
+    mov [di+200h], bh
+    inc di
+    loop move
+
+    int 20h ; finaliza ejecucion 
+
+comment db "Me recupero"
+length equ $-comment
 ;Desarrollo Ejercicio 2
 
 
 ;Desarrollo Ejercicio 3
 
 
-int 20h; Finaliza ejecucion
+
 
