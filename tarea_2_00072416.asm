42892a4cafd78d36b5c1757fdcb607be322aad88
David Massana <00072416@uca.edu.sv>
2020-04-26 20:14:09 -0600

    Sumando notas y obteniendo el promedio

--- tarea_2_00072416.asm
+++ tarea_2_00072416.asm
@@ -1,11 +1,21 @@
 ;Desarrollo Ejercicio 1
 org 100h;
 
+;guardamos las notas
+mov ax, 0d
+
+add ax, 7d
+add ax, 2d
+add ax, 4d
+add ax, 1d
+add ax, 6d
+
+mov cl, 5d
+div cl
 ;Desarrollo Ejercicio 2
-org 100h;
+
 
 ;Desarrollo Ejercicio 3
-org 100h;
 
 
 int 20h; Finaliza ejecucion
